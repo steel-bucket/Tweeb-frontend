709433b262cc92b3a815f646434cd283038d25c1
BlackEyes09 <shuvampatra03@gmail.com>
2024-03-28 18:23:22 +0530

    Fixed the login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,38 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-// import Navb from "./components/Navbar/Navbar.jsx";
-// import { BrowserRouter, Routes, Route } from "react-router-dom"
-// import Auth from "./components/Auth/Auth.jsx"
-//
-//
-// function App() {
-//
-//     return (<div className="App">
-//         <Navb/>
-//         <div className="overlay"></div>
-//         <h1>Welcome to Tweeb</h1>
-//         <p>a open source front to job postings on twitter</p>
-//         <Auth/>
-//     </div>)
-// }
-//
-// export default App
 import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navb from './components/Navbar/Navbar.jsx';
 import Auth from './components/Auth/Auth.jsx';
 
 function Home() {
-    return (<div className={"home"}>
-            <Navb/>
-            <div className="overlay"></div>
-            <h1>Welcome to Tweeb</h1>
-            <p>an open-source front to job postings on Twitter</p>
-        </div>);
+    return (
+        <div style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center", // Add this line to hide vertical scroll
+        }}>
+            <div className='main'>
+                <div className='text'>
+                    <h1>Tweeb</h1>
+                    <p style={{ fontWeight: "lighter" }}>An open-source front to job postings on Twitter</p>
+                </div>
+                <div className='form'>
+                    {
+                        <Auth />
+                    }
+                </div>
+            </div>
+        </div>
+    );
 }
 
 function App() {
     return (
         <div>
-            <Home/>
-            <Auth/>
-        </div>);
+            <Home />
+        </div>
+    );
 }
 
 export default App;
