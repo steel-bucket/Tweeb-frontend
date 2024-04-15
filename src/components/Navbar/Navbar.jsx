5ffd0ee874602b172fac3bba75499966faa95286
BlackEyes09 <shuvampatra03@gmail.com>
2024-04-16 02:26:20 +0530

    changed the ui

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,14 @@
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
-import {FormControl} from "react-bootstrap";
+import { FormControl } from "react-bootstrap";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {useState} from "react";
+import { useState } from "react";
 
 function Navb(props) {
     const search = (e) => {
-        props.setFilters({s: e, page: 1});
+        props.setFilters({ s: e, page: 1 });
     }
     console.log("this is posts")
     console.log(props.posts);
@@ -33,23 +33,16 @@ function Navb(props) {
 
 
     return (
-        <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme="dark">
+        <Navbar expand="lg" className="bg-dark-navy-blue" data-bs-theme="dark" style={{ borderColor: 'blue' }}>
             <Container fluid>
-                <Navbar.Toggle aria-controls="navbarScroll"/>
+                <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
                         className="me-auto my-2 my-lg-0"
-                        style={{maxHeight: '100px'}}
-                        navbarScroll
                     >
                         <Nav.Link href="#action1">About Us</Nav.Link>
                         <Nav.Link href="#action2">Contact</Nav.Link>
                     </Nav>
-                    <Nav className="ms-auto my-2 my-lg-0" style={{maxHeight: '100px'}}
-                         navbarScroll>  {/* Class changed to ms-auto */}
-                        <Nav.Link href="/logout">Logout</Nav.Link>
-                    </Nav>
-
                     <Form className="d-flex">
                         <FormControl
                             type="search"
@@ -59,12 +52,16 @@ function Navb(props) {
                             value={searchTerm}
                             onChange={(e) => setSearchTerm(e.target.value)}
                             onKeyUp={handleKeyUp}
+                            style={{ backgroundColor: '#00388B' }} 
                         />
                         <Button variant="outline-success" onClick={handleSearch} disabled={!searchTerm}>
                             Search
                         </Button>
                     </Form>
-
+                    <Nav className="ms-auto my-2 my-lg-0" style={{ maxHeight: '100px' }}
+                        navbarScroll>  {/* Class changed to ms-auto */}
+                        <Nav.Link href="/logout">Logout</Nav.Link>
+                    </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
--- src/pages/Posts/Posts.jsx
+++ src/pages/Posts/Posts.jsx
@@ -1,22 +1,20 @@
-import {React, useState, useEffect} from "react";
-import './Posts.css';
-import Navb from '../../components/Navbar/Navbar.jsx';
-
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-
+import { React, useState, useEffect } from "react";
+import "./Posts.css";
+import Navb from "../../components/Navbar/Navbar.jsx";
+import { motion } from 'framer-motion';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 function Posts() {
     const [allposts, setAllposts] = useState([]);
     const [filteredposts, setFilteredposts] = useState([]);
     const [filters, setFilters] = useState({
-        s: '',
-        page: 1
+        s: "",
+        page: 1,
     });
-
 
     useEffect(() => {
         const array = [];
@@ -27,22 +25,27 @@ function Posts() {
         if (filters.page) {
             array.push(`page=${filters.page}`);
         }
-        const url = `https://tweeb-api.vercel.app/api/posts/backend?${array.join('&')}`;
+        const url = `https://tweeb-api.vercel.app/api/posts/backend?${array.join(
+            "&"
+        )}`;
         fetch(url)
-            .then(response => response.json())
-            .then(data => {
+            .then((response) => response.json())
+            .then((data) => {
                 setAllposts(filters.page == 1 ? data.data : [...allposts, ...data.data]);
-                setFilteredposts(data.data.filter(post => {
-                    const searchRegex = new RegExp(filters.s.toLowerCase(), 'i');
-                    return searchRegex.test(post.text.toLowerCase())
-                }));
+                setFilteredposts(
+                    data.data.filter((post) => {
+                        const searchRegex = new RegExp(filters.s.toLowerCase(), "i");
+                        return searchRegex.test(post.text.toLowerCase());
+                    })
+                );
             })
-            .catch(error => console.error('Error fetching data:', error));
+            .catch((error) => console.error("Error fetching data:", error));
     }, [filters]);
 
-
     useEffect(() => {
-        let posts = allposts.filter((post) => post.text.toLowerCase().includes(filters.s.toLowerCase()));
+        let posts = allposts.filter((post) =>
+            post.text.toLowerCase().includes(filters.s.toLowerCase())
+        );
         setFilteredposts(posts);
     }, [filters]);
 
@@ -50,47 +53,81 @@ function Posts() {
 
     const load = () => {
         setFilters({
-                ...filters,
-                page: filters.page + 1
-            }
-        );
-    }
+            ...filters,
+            page: filters.page + 1,
+        });
+    };
 
     let button;
     button = (
-        <Button variant="primary" className="d-flex justify-content-center mt-4" onClick={load}>Read More</Button>
+        <Button
+            variant="primary"
+            className="d-flex justify-content-center mt-4"
+            onClick={load}
+        >
+            Read More
+        </Button>
     );
-    return (<div>
-        <div style={{width: '100%'}}>
-            <Navb posts={allposts} filters={filters} setFilters={setFilters}/></div>
+    return (
+        <div style={{backgroundColor: "#000C1F"}}>
+            <div style={{ width: "100%", position: "fixed", top: 0, zIndex: 999, backgroundColor: "#01204E" }}>
+                <Navb posts={allposts} filters={filters} setFilters={setFilters} />
+            </div>
 
-        <Container style={{margin: '0rem', width: '100%', maxWidth: 'none'}}>
-            <Row style={{margin: '0rem', width: '100%'}} className="d-inline-flex">
-                {allposts.map((post) => {
-                    return (<Col xs={12} md={6} lg={3} key={post.id}>
-                            <Card style={{width: '100%', margin: '0.5rem'}}>
-                                <Card.Body>
-                                    <Card.Title>{post.user}</Card.Title>
-                                    <Card.Text>
-                                        {post.text.length > 40 ? (
-                                            <>
-                                                {post.text.substring(0, 100)}...
-                                            </>
-                                        ) : (
-                                            post.text
-                                        )}
-                                    </Card.Text>
-                                    <Button variant="primary">Read More</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    );
-                })}
-                {button}
-            </Row>
-        </Container>
+            <Container
+                style={{ margin: "0rem", width: "100%", maxWidth: "none", overflowX: "hidden", paddingTop: "70px" }}
+            >
+                <Row style={{ margin: "0rem", width: "100%" }} className="d-inline-flex">
+                    {allposts.map((post) => {
+                        return (
+                            <Col xs={12} md={6} lg={3} key={post.id}>
+                                <motion.div
+                                    initial={{ scale: 1 }}
+                                    whileHover={{ scale: 1.1 }}
+                                    whileTap={{ scale: 0.9 }}
+                                    transition={{ duration: 0.2 }}
+                                >
+                                    <Card
+                                        style={{
+                                            width: "100%",
+                                            margin: "0.5rem",
+                                            backgroundColor: "#031735",
+                                            color: "#548AD1",
+                                            border: "2px solid #00418B",
+                                            fontFamily: "Arial",
+                                            
+                                        }}
+                                    >
+                                        <Card.Body>
+                                            <Card.Title style={{ color: "#FFFFFF", fontFamily: "Arial" }}>{post.user}</Card.Title>
+                                            <Card.Text>
+                                                {post.text.length > 40 ? (
+                                                    <>
+                                                        {post.text.substring(0, 100)}...
+                                                    </>
+                                                ) : (
+                                                    post.text
+                                                )}
+                                            </Card.Text>
 
-    </div>);
+                                            <motion.div
+                                                whileHover={{ scale: 1.0 }}
+                                                whileTap={{ scale: 0.9 }}
+                                                transition={{ duration: 0.2 }}
+                                            >
+                                                <Button variant="primary" style={{ backgroundColor: "#003066", borderColor: "#0263D3", boxShadow: "0px 0px 3px #0263D3" }}>Read More</Button>
+                                            </motion.div>
+                                        </Card.Body>
+                                    </Card>
+                                </motion.div>
+                            </Col>
+                        );
+                    })}
+                    {button}
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default Posts;
